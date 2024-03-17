1a25243676d7c3a17e0d1c11a98dd79501f26946
FedeYozora <federicobonfi@gmail.com>
2024-03-17 19:16:50 +0100

    login and register pages

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  showPassword!: boolean;
+  constructor(private authSrv: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  register(registerForm: NgForm) {
+    try {
+      this.authSrv.register(registerForm.value).subscribe();
+    } catch (error: any) {
+      alert(error);
+      this.router.navigate(['/register']);
+    }
+  }
+
+  showPasswordOrNot() {
+    this.showPassword = !this.showPassword;
+  }
+
+  goLogin() {
+    this.router.navigate(['/login']);
+  }
+}
