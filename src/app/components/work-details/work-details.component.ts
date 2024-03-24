cb38b5aa056c5f63da0f3cd13f99a86cc7d6cc35
FedeYozora <federicobonfi@gmail.com>
2024-03-24 19:59:18 +0100

    updated the work and workDetails pages

--- src/app/components/work-details/work-details.component.ts
+++ src/app/components/work-details/work-details.component.ts
@@ -4,6 +4,7 @@ import { CommentsService } from 'src/app/services/comments.service';
 import { WorksService } from 'src/app/services/works.service';
 import { UserService } from 'src/app/services/user.service';
 import { Comment } from 'src/app/models/comment';
+import { Observable, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-work-details',
@@ -12,15 +13,16 @@ import { Comment } from 'src/app/models/comment';
 })
 export class WorkDetailsComponent implements OnInit {
   currentUser: any;
-  works!: any[];
   work: any | undefined;
   comments!: any;
+  userIsAdmin!: boolean;
   commentForm: Comment = {
     workId: Number(this.route.snapshot.params['id']),
     content: '',
     id: 0,
     userId: 0,
     createdAt: new Date(),
+    commentStatus: 'VISIBLE',
   };
 
   constructor(
@@ -31,23 +33,44 @@ export class WorkDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      const workId = Number(params.get('id'));
-      if (!isNaN(workId)) {
-        this.workSrv.getWork(workId).subscribe((work) => {
-          this.work = work;
-          this.getComments(workId);
-        });
-      }
-    });
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const workId = Number(params.get('id'));
+          return this.canActivate().pipe(
+            map((isAdmin) => {
+              if (isAdmin) {
+                this.userIsAdmin = true;
+              }
+              return workId;
+            })
+          );
+        })
+      )
+      .subscribe((workId) => {
+        if (!isNaN(workId) && this.userIsAdmin === true) {
+          this.workSrv.getWork(workId).subscribe((work) => {
+            this.work = work;
+            this.getComments(workId);
+          });
+        } else if (!isNaN(workId)) {
+          {
+            this.workSrv.getWork(workId).subscribe((work) => {
+              this.work = work;
+              this.getVisibleComments(workId);
+            });
+          }
+        }
+      });
+  }
+
+  canActivate(): Observable<boolean> {
+    return this.userSrv.isAdmin();
   }
 
   createComment(comment: Comment) {
     let utente = this.userSrv.getValidUser();
     if (utente) {
-      console.log(utente);
-      // let userId = utente.user.id;
-      // this.commentForm.userId = userId;
     }
     this.commentSrv.createComment(comment).subscribe(() => {
       this.getComments(this.commentForm.workId);
@@ -58,6 +81,28 @@ export class WorkDetailsComponent implements OnInit {
     this.commentSrv.getCommentsForWork(workId).subscribe((comments) => {
       this.comments = comments;
     });
+  }
+
+  getVisibleComments(workId: number): void {
+    this.commentSrv.getVisibleCommentsForWork(workId).subscribe((comments) => {
+      this.comments = comments;
+    });
+  }
+
+  deleteComment(commentId: number) {
+    this.commentSrv.deleteComment(commentId).subscribe(() => {
+      this.getComments(this.commentForm.workId);
+    });
+  }
+
+  toggleCommentStatus(comment: Comment) {
+    if (comment.commentStatus === 'HIDDEN') {
+      comment.commentStatus = 'VISIBLE';
+    } else {
+      comment.commentStatus = 'HIDDEN';
+    }
+    console.log(comment.commentStatus);
+    this.commentSrv.updateComment(comment.id, comment).subscribe(() => {});
   }
 
   speakText(text: string): void {
--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { WorksService } from 'src/app/services/works.service';
+import { UserService } from 'src/app/services/user.service';
 import { AuthData } from 'src/app/models/auth-data';
 
 @Component({
@@ -9,30 +10,36 @@ import { AuthData } from 'src/app/models/auth-data';
   styleUrls: ['./works.component.scss'],
 })
 export class WorksComponent implements OnInit {
-  constructor(private worksService: WorksService) {}
+  constructor(
+    private worksService: WorksService,
+    private userSRV: UserService
+  ) {
+    this.page = 0;
+  }
   works!: any[];
   searchInput!: string;
   userIsAdmin!: boolean;
   userLoggedIn!: AuthData | null;
+  page!: number;
+
+  canActivate(): Observable<boolean> {
+    return this.userSRV.isAdmin();
+  }
 
   ngOnInit(): void {
     this.getWorks();
-    // this.canActivate().subscribe((isAdmin) => {
-    //   if (isAdmin) {
-    //     this.userIsAdmin = true;
-    //   }
-    // });
-  }
-
-  getWorks(): void {
-    this.worksService.getWorks().subscribe((response: any) => {
-      this.works = response.content;
+    this.canActivate().subscribe((isAdmin) => {
+      if (isAdmin) {
+        this.userIsAdmin = true;
+      }
     });
   }
 
-  // canActivate(): Observable<boolean> {
-  //   return this.userService.isAdmin();
-  // }
+  getWorks(): void {
+    this.worksService.getWorks(this.page).subscribe((response: any) => {
+      this.works = response.content;
+    });
+  }
 
   searchWorks(searchValue: string): void {
     this.searchInput = searchValue.trim();
