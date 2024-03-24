5db8e702f57de63f66a83b28007e8996471138c8
FedeYozora <federicobonfi@gmail.com>
2024-03-24 20:00:23 +0100

    created a component to edit the profile of the users

---
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.scss'],
+})
+export class EditProfileComponent implements OnInit {
+  userForm!: NgForm;
+  user: any = {
+    id: 0,
+    name: '',
+    surname: '',
+    avatar: '',
+    username: '',
+    email: '',
+    password: '',
+  };
+  id!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userSRV: UserService,
+    private authSRV: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadUserData();
+  }
+
+  private loadUserData() {
+    this.userSRV.getUser().subscribe((utente) => {
+      this.user = utente;
+    });
+  }
+
+  onSubmit() {
+    this.authSRV.updateUserInfo(this.user).subscribe(
+      () => {
+        this.router.navigate(['']);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  cancel(): void {
+    this.router.navigate(['']);
+  }
+
+  handleImageChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('avatar', file);
+      this.userSRV.uploadImage(formData).subscribe((response) => {
+        this.user.avatar = `${response}`;
+      });
+    }
+  }
+}
