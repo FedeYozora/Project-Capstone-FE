96c0048feeaef4cdbae757c133cf0385476eebab
FedeYozora <federicobonfi@gmail.com>
2024-03-31 22:29:20 +0200

    added pagination

--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -10,6 +10,7 @@ import { AuthData } from 'src/app/models/auth-data';
   styleUrls: ['./works.component.scss'],
 })
 export class WorksComponent implements OnInit {
+  isLoading = false;
   constructor(
     private worksService: WorksService,
     private userSRV: UserService
@@ -21,12 +22,16 @@ export class WorksComponent implements OnInit {
   userIsAdmin!: boolean;
   userLoggedIn!: AuthData | null;
   page!: number;
+  size!: number;
+  totalElements!: number;
+  totalPages!: number;
 
   canActivate(): Observable<boolean> {
     return this.userSRV.isAdmin();
   }
 
   ngOnInit(): void {
+    this.works = [];
     this.canActivate().subscribe((isAdmin) => {
       if (isAdmin) {
         this.userIsAdmin = true;
@@ -39,7 +44,9 @@ export class WorksComponent implements OnInit {
 
   getWorks(): void {
     this.worksService.getWorks(this.page).subscribe((response: any) => {
-      this.works = response.content;
+      this.works = this.works.concat(response.content);
+      this.totalElements = response.totalElements;
+      this.totalPages = response.totalPages;
     });
   }
 
@@ -47,7 +54,9 @@ export class WorksComponent implements OnInit {
     this.worksService
       .getWorksVisibleComments(this.page)
       .subscribe((response: any) => {
-        this.works = response.content;
+        this.works = this.works.concat(response.content);
+        this.totalElements = response.totalElements;
+        this.totalPages = response.totalPages;
       });
   }
 
@@ -61,5 +70,18 @@ export class WorksComponent implements OnInit {
     this.works = this.works.filter((work) =>
       work.name.toLowerCase().includes(this.searchInput.toLowerCase())
     );
+  }
+
+  loadNextPage(): void {
+    this.page++;
+    if (this.userIsAdmin === true) {
+      this.getWorks();
+    } else {
+      this.getWorksVisibleComments();
+    }
+  }
+
+  hasMorePages(): boolean {
+    return this.page < this.totalPages - 1;
   }
 }
